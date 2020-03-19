088af13f3751b58095d5430f1984adfdddefbfe7
cihatata <omercihatataa@gmail.com>
2020-03-19 21:58:40 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "./assets/css/skeleton";
+#app {
+
+}
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+import routerOptions from './router/route'
+import  storeOptions from './stores/store'
+
+const { state, getters, mutations, actions } = storeOptions;
+const router = new VueRouter(routerOptions);
+const store = new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions,
+});
+
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
